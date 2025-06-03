dd8c39d5457a20dbd87faedf7195aa17f947d771
olivermjmj <olivermjmj@outlook.dk>
2025-06-03 16:55:45 +0200

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.nation.dungeon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.nation.dungeon to javafx.fxml;
+    exports com.nation.dungeon;
+}

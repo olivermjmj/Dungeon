54926619b168eb4720a53f92bb840007d2c6182c
olivermjmj <olivermjmj@outlook.dk>
2025-07-22 12:41:25 +0200

    A user is now able to register and login with saved data.

--- src/main/java/com/nation/dungeon/controllers/LoginRegisterMenu_Controller.java
+++ src/main/java/com/nation/dungeon/controllers/LoginRegisterMenu_Controller.java
@@ -5,14 +5,26 @@ import com.nation.dungeon.util.GameManager;
 import com.nation.dungeon.util.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 public class LoginRegisterMenu_Controller {
+
+    private int level;
+    private double strength;
+    private double currentHp;
+    private double maxHp;
+    private double defence;
+    private double currentMana;
+    private double maxMana;
+    private long money;
+
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
+    @FXML private Label errorLabel;
 
     @FXML private Button login;
     @FXML private Button register;
@@ -32,26 +44,30 @@ public class LoginRegisterMenu_Controller {
         login.setOnAction(action -> {
 
             if(tryLogin()) {
-
-                String currentUserName = GameManager.get().getPlayer().getUsername();
-
-                GameManager.get().getPlayer().setLevel(GameManager.get().databaseManager().getUserLevel(currentUserName));
-                GameManager.get().getPlayer().setCurrentHp(GameManager.get().databaseManager().getUserCurrentHP(currentUserName));
-                GameManager.get().getPlayer().setMaxHp(GameManager.get().databaseManager().getUserMaxHP(currentUserName));
-                GameManager.get().getPlayer().setCurrentMana(GameManager.get().databaseManager().getUserCurrentMana(currentUserName));
-                GameManager.get().getPlayer().setMaxMana(GameManager.get().databaseManager().getUserMaxMana(currentUserName));
-                GameManager.get().getPlayer().setDefence(GameManager.get().databaseManager().getUserDefence(currentUserName));
-                GameManager.get().getPlayer().setMoney(GameManager.get().databaseManager().getUserMoney(currentUserName));
-                GameManager.get().getPlayer().setStrength(GameManager.get().databaseManager().getUserStrength(currentUserName));
-                SceneManager.switchScene("createCharacterMenu.fxml");
+                if(this.level == 1 && strength == 1.0 && currentHp == 100.0 && maxHp == 100.0 && defence == 1.0 && currentMana == 100.0 && maxMana == 100.0 && money == 1000) {
+                    SceneManager.switchScene("createCharacterMenu.fxml");
+                } else {
+                    SceneManager.switchScene("cityMenu.fxml");
+                }
+            } else {
+                //Missing
             }
         });
 
         register.setOnAction(action -> {
             if(tryRegister()) {
-
+                usernameField.clear();
+                passwordField.clear();
+                errorLabel.setVisible(false);
+            } else if(GameManager.get().databaseManager().doesUserExist(usernameField.getText(), passwordField.getText())){
+                errorLabel.setStyle("-fx-text-fill: red;");
+                errorLabel.setText("User already exists");
+                errorLabel.setVisible(true);
+                //To be continued
             } else {
-
+                errorLabel.setStyle("-fx-text-fill: red;");
+                errorLabel.setText("username and password must be with equal to 4 - 10 characters");
+                errorLabel.setVisible(true);
             }
         });
 
@@ -59,7 +75,53 @@ public class LoginRegisterMenu_Controller {
             SceneManager.switchScene("startMenu.fxml");
         });
 
-        //Fxml styling
+        //fxml button styling
+        fxmlButtonReactStyling();
+    }
+
+    private boolean tryLogin() {
+        if (usernameField != null && usernameField.getLength() <= 10) {
+            if (passwordField != null && passwordField.getLength() <= 10) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+
+                if (GameManager.get().databaseManager().doesUserExist(username, password)) {
+                    // Hent alle stats fra databasen
+                    this.level = GameManager.get().databaseManager().getUserLevel(username);
+                    this.strength = GameManager.get().databaseManager().getUserStrength(username);
+                    this.currentHp = GameManager.get().databaseManager().getUserCurrentHP(username);
+                    this.maxHp = GameManager.get().databaseManager().getUserMaxHP(username);
+                    this.defence = GameManager.get().databaseManager().getUserDefence(username);
+                    this.currentMana = GameManager.get().databaseManager().getUserCurrentMana(username);
+                    this.maxMana = GameManager.get().databaseManager().getUserMaxMana(username);
+                    this.money = GameManager.get().databaseManager().getUserMoney(username);
+
+                    //Creates player with all values from the database
+                    Player newPlayer = new Player(this.level, username, password, this.strength, this.currentHp, this.maxHp, this.defence, this.currentMana, this.maxMana, this.money);
+
+                    //Sets player in GameManager
+                    GameManager.get().setPlayer(newPlayer);
+
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean tryRegister() {
+        if(usernameField != null && (usernameField.getLength() <= 10 && usernameField.getLength() >= 4)) {
+            if (passwordField != null && (passwordField.getLength() <= 10 && passwordField.getLength() >= 4)) {
+                GameManager.get().databaseManager().createUser(usernameField.getText() ,passwordField.getText());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //A function made for the purpose, of making the buttons light up, when being hovered over.
+    private void fxmlButtonReactStyling() {
         login.setOnMouseEntered(action -> {
             login.setStyle("""
                     -fx-background-color: #FFD700;
@@ -146,46 +208,5 @@ public class LoginRegisterMenu_Controller {
                                        -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.6), 5, 0, 0, 0);
                     """);
         });
-    }
-
-    private boolean tryLogin() {
-        if (usernameField != null && usernameField.getLength() <= 10) {
-            if (passwordField != null && passwordField.getLength() <= 10) {
-                String username = usernameField.getText();
-                String password = passwordField.getText();
-
-                if (GameManager.get().databaseManager().doesUserExist(username, password)) {
-                    // Hent alle stats fra databasen
-                    int level = GameManager.get().databaseManager().getUserLevel(username);
-                    double strength = GameManager.get().databaseManager().getUserStrength(username);
-                    double currentHp = GameManager.get().databaseManager().getUserCurrentHP(username);
-                    double maxHp = GameManager.get().databaseManager().getUserMaxHP(username);
-                    double defence = GameManager.get().databaseManager().getUserDefence(username);
-                    double currentMana = GameManager.get().databaseManager().getUserCurrentMana(username);
-                    double maxMana = GameManager.get().databaseManager().getUserMaxMana(username);
-                    long money = GameManager.get().databaseManager().getUserMoney(username);
-
-                    //Creates player with all values from the database
-                    Player newPlayer = new Player(level, username, password, strength, currentHp, maxHp, defence, currentMana, maxMana, money);
-
-                    //Sets player in GameManager
-                    GameManager.get().setPlayer(newPlayer);
-
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-    private boolean tryRegister() {
-        if(usernameField != null && usernameField.getLength() <= 10) {
-            if (passwordField != null && passwordField.getLength() <= 10) {
-                GameManager.get().databaseManager().createUser(usernameField.getText() ,passwordField.getText());
-                return true;
-            }
-        }
-        return false;
     }
 }

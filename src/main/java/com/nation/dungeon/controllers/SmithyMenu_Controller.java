8e8f708764ec32d4d0fd30797ceb5263d13c1aca
olivermjmj <olivermjmj@outlook.dk>
2025-08-04 22:04:59 +0200

    SmithyMenu image views

--- src/main/java/com/nation/dungeon/controllers/SmithyMenu_Controller.java
+++ src/main/java/com/nation/dungeon/controllers/SmithyMenu_Controller.java
@@ -1,7 +1,9 @@
 package com.nation.dungeon.controllers;
 
+import com.nation.dungeon.merchant.Item;
 import com.nation.dungeon.util.GameManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -18,9 +20,19 @@ public class SmithyMenu_Controller {
     @FXML private ImageView sword4;
     @FXML private ImageView sword5;
 
+    @FXML private TextField sword0Description;
+    @FXML private TextField sword1Description;
+    @FXML private TextField sword2Description;
+    @FXML private TextField sword3Description;
+    @FXML private TextField sword4Description;
+    @FXML private TextField sword5Description;
+
     @FXML private Pane uiLayer;
     @FXML private ImageView background;
     @FXML private StackPane root;
+
+    private ImageView[] swordImageArray;
+    private TextField[] swordDescriptionArray;
 
     @FXML
     public void initialize() {
@@ -32,13 +44,29 @@ public class SmithyMenu_Controller {
         uiLayer.prefWidthProperty().bind(root.widthProperty());
         uiLayer.prefHeightProperty().bind(root.heightProperty());
 
-        //itemCombiner.items has 6 elements from the beginning
-        sword0.setImage(GameManager.get().itemCombiner().items.get(0).getImage());
-        sword1.setImage(GameManager.get().itemCombiner().items.get(1).getImage());
-        sword2.setImage(GameManager.get().itemCombiner().items.get(2).getImage());
-        sword3.setImage(GameManager.get().itemCombiner().items.get(3).getImage());
-        sword4.setImage(GameManager.get().itemCombiner().items.get(4).getImage());
-        sword5.setImage(GameManager.get().itemCombiner().items.get(5).getImage());
+        //Sets the first 6 weapon pictures
+        swordImageArray = new ImageView[] {sword0, sword1, sword2, sword3, sword4, sword5};
+
+        for(int i = 0; i < swordImageArray.length && i < GameManager.get().itemCombiner().items.size(); i++) {
+            swordImageArray[i].setImage(GameManager.get().itemCombiner().items.get(i).getImage());
+        }
+
+        //Sets the first 6 weapon descriptions
+        setFirstSixWeaponNames();
+
+        //Sets the textFields to the string value of the weapon they are describing
+        for(Item item : GameManager.get().itemCombiner().items) {
+            sword0Description.setText(item.getDescription());
+        }
+    }
+
+    private void setFirstSixWeaponNames() {
+        GameManager.get().itemCombiner().items.get(0).setDescription("Trodden wooden sword");
+        GameManager.get().itemCombiner().items.get(1).setDescription("Rusty bronze sword");
+        GameManager.get().itemCombiner().items.get(2).setDescription("Thoroughly worn metal sword");
+        GameManager.get().itemCombiner().items.get(3).setDescription("Fine iron sword");
+        GameManager.get().itemCombiner().items.get(4).setDescription("great damascus steel sword");
+        GameManager.get().itemCombiner().items.get(5).setDescription("Dragon steel sword");
     }
 
 }
